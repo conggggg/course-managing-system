7b85c780676006135f8ed4fd4e5ee1bf060ffff8
mmLMZ <lmo_zhen@163.com>
2021-01-06 16:18:16 +0800

    1

---
+++ pages/pages/Teachers/Teacher_main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "获取教师编号";
+    document.getElementById("demo2").innerHTML = "获取教师姓名";
+    document.getElementById("demo3").innerHTML = "获取教师性别";
+}
