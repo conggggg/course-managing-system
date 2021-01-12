62a35317fd56c28c0ffbbbabfa2b877b84571741
Zeromes <178800633@qq.com>
2021-01-12 14:46:27 +0800

    修正部分路径和接口

--- pages/pages/Manager/Manager_main/Manager_all.js
+++ pages/pages/Manager/Manager_main/Manager_all.js
@@ -1,3 +1,9 @@
+//点击返回按钮,返回到主页面
+function return_to_main() {
+	window.location.href = '../Manager_main/Manager_main.html'
+}
+
+//管理员登出
 function logout() {
 	//删除cookie
 	deleteCookie("username");
@@ -32,9 +38,12 @@ function course_add() {
 			console.log(re);
 			if (re.result) {
 				alert("添加成功！");
+				setTimeout(function() {
+					window.location.assign("../Manager_main/Manager_main.html")
+				},1000)
 			}
 			else {
-				alert("添加失败！");
+				alert("添加失败！请检查输入是否有误！");
 			}
 		}
 		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
@@ -58,17 +67,6 @@ function course_change() {
 	alert("修改课程");//修改课程函数,自己修改实现?
 }
 
-//点击课程信息表的一行,则获取该行的属性值
-function get_course_info() {
-	var course_id = "", course_name = "", course_mark = "", course_time = "", course_type = "";//初始化变量,获取输入的值
-	course_id = document.getElementById("Course_Id");//获取各个属性的值
-	course_name = document.getElementById("Course_Name");
-	course_mark = document.getElementById("Course_Mark");
-	course_time = document.getElementById("Course_Time");
-	course_type = document.getElementById("Course_Type");
-	alert("获取到该行的信息");
-
-}
 //根据信息,或者直接获取课程信息表
 function get_course() {
 	//创建AJAX对象
@@ -109,6 +107,14 @@ function get_course() {
 				let courseTypeNode = document.createTextNode(re.data[x].courseType);
 				courseType.appendChild(courseTypeNode);
 				tr.appendChild(courseType)
+				//添加删除按钮
+				let deleteTd = document.createElement("td");
+				let deleteButton = document.createElement("button");
+				let deleteButtonNode = document.createTextNode("删除");
+				deleteButton.appendChild(deleteButtonNode);
+				deleteButton.setAttribute("onclick","delete_course(this)");
+				deleteTd.appendChild(deleteButton);
+				tr.appendChild(deleteTd)
 				//将当前栏插进表格
 				element.appendChild(tr);
 			}
@@ -122,20 +128,11 @@ function get_course() {
 	xmlhttp.send();
 }
 
-//点击添加学生按钮,实现添加学生功能,更新数据库学生信息表
-function student_add() {
-	//获取各个属性的值
-	var type = "student";
-	var student_id = document.getElementById("Student_Id").value;
-	var student_name = document.getElementById("Student_Name").value;
-	var student_sex = document.getElementById("Student_sex").value;
-	var student_grade = document.getElementById("Student_Grade").value;
-	var zhuanye = document.getElementById("zhuanye").value;
+//删除课程
+function delete_course(button){
+	console.log(button.parentNode.parentNode.children[0].innerHTML)//选中的课程号
 	var data = {
-		Sid: student_id,
-		Sname: student_name,
-		Ssex: student_sex,
-		Sclass: student_grade,
+		courseid: button.parentNode.parentNode.children[0].innerHTML,
 	}
 	//创建AJAX对象
 	var xmlhttp = new XMLHttpRequest();
@@ -145,25 +142,38 @@ function student_add() {
 			//解析json对象
 			let re = JSON.parse(xmlhttp.responseText);
 			console.log(re);
+			if (re.result) {
+				//删除视图层的表格项
+				button.parentNode.parentNode.parentNode.removeChild(button.parentNode.parentNode)
+				alert("删除成功！");
+			}
+			else {
+				alert("删除失败！");
+			}
 		}
 		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
 			alert("网络错误！");
 		}
 	};
 	//设置并提交申请
-	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/login", true);
-	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlhttp.send("type=" + type + "&data=" + JSON.stringify(data));
+	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/managerdeletecourse", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send("data=" + JSON.stringify(data));
 }
 
-//点击修改学生信息按钮,实现修改学生信息功能,更新数据库学生信息表
-function student_changeinfo() {
-	var student_id = document.getElementById("Student_Id");//获取各个属性的值
-	var student_name = document.getElementById("Student_Name");
-	var student_sex = document.getElementById("Student_sex");
-	var student_grade = document.getElementById("Student_Grade");
-	var zhuanye = document.getElementById("zhuanye");
-
+//点击添加学生按钮,实现添加学生功能,更新数据库学生信息表
+function student_add() {
+	//获取各个属性的值
+	var student_name = document.getElementById("Student_Name").value;
+	var student_id = document.getElementById("Student_Id").value;
+	var student_sex = document.getElementById("Student_sex").value;
+	var student_class = document.getElementById("Student_Class").value;
+	var data = {
+		studentname: student_name,
+		studentid: student_id,
+		studentsex: student_sex,
+		classname: student_class,
+	}
 	//创建AJAX对象
 	var xmlhttp = new XMLHttpRequest();
 	//服务器返回数据回调函数
@@ -172,12 +182,98 @@ function student_changeinfo() {
 			//解析json对象
 			let re = JSON.parse(xmlhttp.responseText);
 			console.log(re);
+			if (re.result) {
+				alert("添加成功！");
+				setTimeout(function() {
+					window.location.assign("../Manager_main/Manager_main.html")
+				},1000)
+			}
+			else {
+				alert("添加失败！请检查输入是否有误！");
+			}
+		}
+		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+			alert("网络错误！");
 		}
 	};
 	//设置并提交申请
-	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/login", true);
-	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlhttp.send("username=" + username + "&password=" + password);
+	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/manageraddstudent", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send("data=" + JSON.stringify(data));
+}
+
+//点击修改学生信息按钮,实现修改学生信息功能,更新数据库学生信息表
+function student_changeinfo() {
+	//获取各个属性的值
+	var student_name = document.getElementById("Student_Name").value;
+	var student_id = document.getElementById("Student_Id").value;
+	var student_sex = document.getElementById("Student_sex").value;
+	var student_class = document.getElementById("Student_Class").value;
+	var data = {
+		studentname: student_name,
+		studentid: student_id,
+		studentsex: student_sex,
+		classname: student_class,
+	}
+	//创建AJAX对象
+	var xmlhttp = new XMLHttpRequest();
+	//服务器返回数据回调函数
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			//解析json对象
+			let re = JSON.parse(xmlhttp.responseText);
+			console.log(re);
+			if (re.result) {
+				alert("修改成功！");
+				setTimeout(function() {
+					window.location.assign("../Manager_main/Manager_main.html")
+				},1000)
+			}
+			else {
+				alert("修改失败！请检查输入是否有误！");
+			}
+		}
+		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+			alert("网络错误！");
+		}
+	};
+	//设置并提交申请
+	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/managerupdatestudent", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send("data=" + JSON.stringify(data));
+}
+
+//删除学生信息
+function delete_student(button) {
+	console.log(button.parentNode.parentNode.children[0].innerHTML)//选中的学生学号
+	var data = {
+		studentid: button.parentNode.parentNode.children[0].innerHTML,
+	}
+	//创建AJAX对象
+	var xmlhttp = new XMLHttpRequest();
+	//服务器返回数据回调函数
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			//解析json对象
+			let re = JSON.parse(xmlhttp.responseText);
+			console.log(re);
+			if (re.result) {
+				//删除视图层的表格项
+				button.parentNode.parentNode.parentNode.removeChild(button.parentNode.parentNode)
+				alert("删除成功！");
+			}
+			else {
+				alert("删除失败！");
+			}
+		}
+		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+			alert("网络错误！");
+		}
+	};
+	//设置并提交申请
+	xmlhttp.open("POST", "http://172.18.41.15:8080/testdoc/managerdeletestudent", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send("data=" + JSON.stringify(data));
 }
 
 //点击添加教师按钮,实现添加教师功能,更新数据库教师信息表
@@ -202,9 +298,70 @@ function teacher_changeinfo() {
 	alert("修改教师信息");
 }
 
-//查询刷新学生表格
-function get_newstudent() {
-	alert("刷新学生信息表");
+//获取学生表格
+function get_student() {
+	//创建AJAX对象
+	var xmlhttp = new XMLHttpRequest();
+	//服务器返回数据回调函数
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			//解析json对象
+			let re = JSON.parse(xmlhttp.responseText);
+			console.log(re);
+			//获取表格DOM对象
+			let element = document.getElementById("studentTable");
+			for (let x in re.data) {
+				//表格栏DOM对象
+				let tr = document.createElement("tr");
+				//插入学号
+				let studentId = document.createElement("td");
+				let studentIdNode = document.createTextNode(re.data[x].studentId);
+				studentId.appendChild(studentIdNode);
+				tr.appendChild(studentId)
+				//插入姓名
+				let studentName = document.createElement("td");
+				let studentNameNode = document.createTextNode(re.data[x].studentName);
+				studentName.appendChild(studentNameNode);
+				tr.appendChild(studentName)
+				//插入性别
+				let studentSex = document.createElement("td");
+				let studentSexNode = document.createTextNode(re.data[x].studentSex);
+				studentSex.appendChild(studentSexNode);
+				tr.appendChild(studentSex)
+				//插入年级
+				let grade = document.createElement("td");
+				let gradeNode = document.createTextNode(re.data[x].grade);
+				grade.appendChild(gradeNode);
+				tr.appendChild(grade)
+				//插入专业
+				let profession = document.createElement("td");
+				let professionNode = document.createTextNode(re.data[x].profession);
+				profession.appendChild(professionNode);
+				tr.appendChild(profession)
+				//插入班级
+				let className = document.createElement("td");
+				let classNameNode = document.createTextNode(re.data[x].className);
+				className.appendChild(classNameNode);
+				tr.appendChild(className)
+				//添加删除按钮
+				let deleteTd = document.createElement("td");
+				let deleteButton = document.createElement("button");
+				let deleteButtonNode = document.createTextNode("删除");
+				deleteButton.appendChild(deleteButtonNode);
+				deleteButton.setAttribute("onclick","delete_student(this)");
+				deleteTd.appendChild(deleteButton);
+				tr.appendChild(deleteTd)
+				//将当前栏插进表格
+				element.appendChild(tr);
+			}
+		}
+		else if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+			alert("网络错误！");
+		}
+	};
+	//设置并提交申请
+	xmlhttp.open("GET", "http://172.18.41.15:8080/testdoc/managerquerystudent", true);
+	xmlhttp.send();
 }
 
 //查询刷新教师表格
@@ -225,11 +382,6 @@ function get_student_chart() {
 //查询教师授课课程信息
 function get_teach_course() {
 	alert("刷新教师授课课程表");
-}
-
-//点击返回按钮,返回到主页面
-function return_to_main() {
-	window.location.href = '../Manager_main/Manager_main.html'
 }
 
 //学生信息表格选中某行,获取某行信息
