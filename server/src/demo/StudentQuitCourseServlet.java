3463e5a4eafde4a8b084d99f04726f96e3c69d2d
1osing <30589877+1osing@users.noreply.github.com>
2021-01-13 17:57:41 +0800

    补充注解

--- server/src/demo/StudentQuitCourseServlet.java
+++ server/src/demo/StudentQuitCourseServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "StudentQuitCourseServlet")
+@WebServlet(name = "StudentQuitCourseServlet",urlPatterns = "/studentquitcourse")
 public class StudentQuitCourseServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin","*");
